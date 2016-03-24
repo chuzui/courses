48b3d8ad0af482352d68955e4b1922b80bbd1e35
chuzui <chuqi77@gmail.com>
2016-03-24 15:32:33 +0800

    Finish keith numbers

--- cs106x/keith-numbers/src/keith-numbers.cpp
+++ cs106x/keith-numbers/src/keith-numbers.cpp
@@ -4,11 +4,13 @@
  */
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 #include "console.h"
 #include "simpio.h"
-#include "vector.h"
+#include "error.h"
+#include "strlib.h"
 
 void introduce() {
 	ifstream infile("keith-numbers.txt");
@@ -26,13 +28,45 @@ void introduce() {
 	cout << endl;
 }
 
+bool isKeithNumber(vector<int>& seq, int n)
+{
+	string str = integerToString(n);
+
+	int numSize = str.size();
+	for (char c : str)
+		seq.push_back(c - '0');
+
+	while (seq.back() < n)
+	{
+		int next = 0;
+
+		for (int i = seq.size() - numSize; i < seq.size(); ++i)
+		{
+			next += seq[i];
+		}
+
+		seq.push_back(next);
+	}
+
+	return seq.back() == n;
+}
+
 int main() {
 	introduce();
 	for (int n = 1; n < 10000; n++) {
 		// replace the following with the logic needed to print
 		// only Keith numbers, along with the sequence that
 		// proves it's a Keith number (as described in the section handout)
-		cout << n << endl;
+		vector<int> seq;
+
+		if (isKeithNumber(seq, n))
+		{
+			cout << n << ": [";
+			for (int i = 0; i < seq.size() - 1; ++i) {
+				cout << seq[i] << ", ";
+			}
+			cout << n <<  "]" << endl;
+		}
 	}
 	
 	cout << endl;

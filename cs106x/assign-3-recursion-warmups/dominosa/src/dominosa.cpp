6cfe61dd2aee225b0248f084fb60b88d28190e07
chuzui <chuqi77@gmail.com>
2016-03-25 23:52:17 +0800

    Init cs106x ass3

---
+++ cs106x/assign-3-recursion-warmups/dominosa/src/dominosa.cpp
@@ -0,0 +1,65 @@
+/**
+ * File: dominosa.cpp
+ * ------------------
+ * This animates the brute-force discovery of a solution
+ * to a 2 x n dominosa board.
+ */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+#include "simpio.h"
+#include "grid.h"
+#include "random.h"
+#include "dominosa-graphics.h"
+
+static void welcome() {
+	cout << "Here we'll illustrate the use of recursive backtracking to" << endl;
+	cout << "discover a solution to various 2 x n Dominosa boards.  In some" << endl;
+	cout << "cases there won't be any solutions, and in the cases where there are" << endl;
+	cout << "multiple solutions, we'll just find one of them." << endl;
+	cout << endl;
+}
+
+static int getIntegerInRange(string prompt, int low, int high) {
+	while (true) {
+		int response = getInteger(prompt);
+		if (response == 0 || (response >= low && response <= high)) return response;
+		cout << "Sorry, but I can't use that number." << endl;
+	}
+}
+
+static void populateBoard(Grid<int>& board, int low, int high) {
+	for (int row = 0; row < board.numRows(); row++) {
+		for (int col = 0; col < board.numCols(); col++) {
+			board[row][col] = randomInteger(low, high);
+		}
+	}
+}
+
+static bool canSolveBoard(DominosaDisplay& display, Grid<int>& board) {
+    // replace this with your own solution, which will almost certainly be a wrapper function
+	return false;
+}
+
+int main() {
+	DominosaDisplay display;
+	welcome();
+	while (true) {
+		int numColumns = getIntegerInRange("How many columns? [0 to exit]: ", 9, 25);
+		if (numColumns == 0) break;
+		Grid<int> board(2, numColumns);
+		populateBoard(board, 1, ceil(2 * sqrt(numColumns)));
+		display.drawBoard(board);
+		if (canSolveBoard(display, board)) {
+			cout << "The board can be solved, and one such solution is drawn above." << endl;
+		} else {
+			cout << "This board you see can't be solved." << endl;
+		}
+	}
+	
+	cout << "Okay, thanks for watching, and come back soon." << endl;
+    cout << "Click the mouse anywhere in the window to exit." << endl;
+	return 0;
+}
---
+++ cs106x/assign-3-recursion-warmups/vampire-numbers/src/vampire-numbers.cpp
@@ -0,0 +1,49 @@
+/**
+ * File: vampire-numbers.cpp
+ * -------------------------
+ * Provides a program that repeatedly prompts the user for positive
+ * numbers with an even number of digits and informs them
+ * whether the number is a Vampire number or not.
+ */
+
+#include <string>    // for string
+#include <iostream>  // for cout, endl
+using namespace std;
+
+#include "console.h" 
+#include "simpio.h" // for getInteger
+#include "strlib.h" // for stringToInteger, integerToString
+
+static int getPossibleVampireNumber() {
+    while (true) {
+        int response = getInteger("Enter a positive integer [or 0 to quit]: ");
+        if (response >= 0) return response;
+        cout << "Ah, sorry.  I need a nonnegative response.  Try again... " << endl;
+    }
+}
+
+static bool isVampireNumber(int number, int& first, int& second) {
+    // replace this line with your own implementation.  You will want
+    // to implement this as a wrapper around a second version of isVampireNumber
+    // that does the actual recursion.
+    return false;
+}
+
+int main() {
+    cout << "Here's a program that tells you whether or not a "
+         << "number you enter is Vampire." << endl << endl;
+    while (true) {
+        int number = getPossibleVampireNumber();
+        if (number == 0) break;
+        int first, second;
+        if (isVampireNumber(number, first, second)) {
+            cout << "Woo! " << number << " is a Vampire number, and "
+                 << first << " and " << second << " are its fangs." << endl << endl;
+        } else {
+            cout << "Nope! The number " << number << " isn't Vampire." << endl << endl;
+        }
+    }
+    cout << endl;
+    cout << "All done!" << endl;
+    return 0;
+}

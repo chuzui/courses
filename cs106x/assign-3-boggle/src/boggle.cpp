58d8d48edf304bdb44c6d3f01b523f522680f151
chuzui <chuqi77@gmail.com>
2016-03-26 19:37:02 +0800

    Init boggle

---
+++ cs106x/assign-3-boggle/src/boggle.cpp
@@ -0,0 +1,76 @@
+/**
+ * File: boggle.cpp
+ * ----------------
+ * Implements the game of Boggle.
+ */
+ 
+#include <cctype>
+#include <iostream>
+using namespace std;
+
+#include "simpio.h"
+#include "lexicon.h"
+#include "gwindow.h"
+#include "gboggle.h"
+
+const int kBoggleWindowWidth = 650;
+const int kBoggleWindowHeight = 350;
+
+const string kStandardCubes[16] = {
+   "AAEEGN", "ABBJOO", "ACHOPS", "AFFKPS",
+   "AOOTTW", "CIMOTU", "DEILRX", "DELRVY",
+   "DISTTY", "EEGHNW", "EEINSU", "EHRTVW",
+   "EIOSST", "ELRTTY", "HIMNQU", "HLNNRZ"
+};
+
+const string kBigBoggleCubes[25] = {
+   "AAAFRS", "AAEEEE", "AAFIRS", "ADENNN", "AEEEEM",
+   "AEEGMU", "AEGMNN", "AFIRSY", "BJKQXZ", "CCNSTW",
+   "CEIILT", "CEILPT", "CEIPST", "DDLNOR", "DDHNOT",
+   "DHHLOR", "DHLNOR", "EIIITT", "EMOTTT", "ENSSSU",
+   "FIPRSY", "GORRVW", "HIPRRY", "NOOTUW", "OOOTTU"
+};
+
+static void welcome() {
+    cout << "Welcome!  You're about to play an intense game ";
+    cout << "of mind-numbing Boggle.  The good news is that ";
+    cout << "you might improve your vocabulary a bit.  The ";
+    cout << "bad news is that you're probably going to lose ";
+    cout << "miserably to this little dictionary-toting hunk ";
+    cout << "of silicon.  If only YOU had a gig of RAM..." << endl << endl;
+}
+
+static void giveInstructions() {
+    cout << endl;
+    cout << "The boggle board is a grid onto which I ";
+    cout << "I will randomly distribute cubes. These ";
+    cout << "6-sided cubes have letters rather than ";
+    cout << "numbers on the faces, creating a grid of ";
+    cout << "letters on which you try to form words. ";
+    cout << "You go first, entering all the words you can ";
+    cout << "find that are formed by tracing adjoining ";
+    cout << "letters. Two letters adjoin if they are next ";
+    cout << "to each other horizontally, vertically, or ";
+    cout << "diagonally. A letter can only be used once ";
+    cout << "in each word. Words must be at least four ";
+    cout << "letters long and can be counted only once. ";
+    cout << "You score points based on word length: a ";
+    cout << "4-letter word is worth 1 point, 5-letters ";
+    cout << "earn 2 points, and so on. After your puny ";
+    cout << "brain is exhausted, I, the supercomputer, ";
+    cout << "will find all the remaining words and double ";
+    cout << "or triple your paltry score." << endl << endl;
+    cout << "Hit return when you're ready...";
+    getLine();
+}
+
+int main() {
+   GWindow gw(kBoggleWindowWidth, kBoggleWindowHeight);
+   Lexicon english("dictionary.txt");
+   initGBoggle(gw);
+   welcome();
+   if (getYesOrNo("Do you need instructions?")) {
+      giveInstructions();
+   }
+   return 0;
+}

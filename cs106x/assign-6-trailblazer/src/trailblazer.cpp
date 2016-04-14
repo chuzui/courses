fe826d04218018d1208ad11a0714602045ba41d7
chuzui <chuqi77@gmail.com>
2016-04-15 00:15:32 +0800

    Finish bfs

--- cs106x/assign-6-trailblazer/src/trailblazer.cpp
+++ cs106x/assign-6-trailblazer/src/trailblazer.cpp
@@ -5,6 +5,9 @@
 
 #include "costs.h"
 #include "trailblazer.h"
+#include "queue.h"
+#include <map>
+
 // TODO: include any other headers you need; remove this comment
 using namespace std;
 
@@ -18,11 +21,38 @@ Vector<Vertex*> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) 
 }
 
 Vector<Vertex*> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
+    graph.resetData();
     Vector<Vertex*> path;
+    Queue<Vertex*> q;
+    cout << "asdasdsadsa" << endl;
+    q.enqueue(start);
+    start->visited = true;
+
+    while(!q.isEmpty())
+    {
+        Vertex* v = q.dequeue();
+
+        if (v == end)
+            break;
+
+        for (auto edge : v->edges)
+        {
+            Vertex* next_v = edge->finish;
+            if (!next_v->visited)
+            {
+                q.enqueue(next_v);
+                next_v->previous = v;
+                next_v->visited = true;
+            }
+        }
+    }
+
+    Vertex* path_v = end;
+    while (path_v && path_v->visited)
+    {
+        path.insert(0, path_v);
+        path_v = path_v->previous;
+    }
     return path;
 }
 

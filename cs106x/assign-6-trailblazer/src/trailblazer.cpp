e65748d25092ed2b1a41b22a90b98108953b212d
chuzui <chuqi77@gmail.com>
2016-04-15 17:11:13 +0800

    Finish Dijkstra

--- cs106x/assign-6-trailblazer/src/trailblazer.cpp
+++ cs106x/assign-6-trailblazer/src/trailblazer.cpp
@@ -7,6 +7,7 @@
 #include "trailblazer.h"
 #include "queue.h"
 #include "stack.h"
+#include "pqueue.h"
 #include <map>
 
 // TODO: include any other headers you need; remove this comment
@@ -16,7 +17,6 @@ Vector<Vertex*> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) 
     graph.resetData();
     Vector<Vertex*> path;
     Stack<Vertex*> q;
-    cout << "asdasdsadsa" << endl;
     q.push(start);
     start->visited = true;
 
@@ -52,7 +52,6 @@ Vector<Vertex*> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end
     graph.resetData();
     Vector<Vertex*> path;
     Queue<Vertex*> q;
-    cout << "asdasdsadsa" << endl;
     q.enqueue(start);
     start->visited = true;
 
@@ -85,19 +84,61 @@ Vector<Vertex*> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end
 }
 
 Vector<Vertex*> dijkstrasAlgorithm(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
+    graph.resetData();
+    PriorityQueue<Vertex*> pqueue;
     Vector<Vertex*> path;
+
+    pqueue.enqueue(start, 0);
+    start->cost = 0.0;
+    start->setColor(YELLOW);
+
+    while(!pqueue.isEmpty())
+    {
+        Vertex* v = pqueue.dequeue();
+        v->setColor(GREEN);
+
+        if (v == end)
+            break;
+
+        for (auto edge : v->edges)
+        {
+            Vertex* next_v = edge->finish;
+            if (next_v->getColor() == 0)
+            {
+                next_v->setColor(YELLOW);
+                double next_cost = v->cost + edge->cost;
+                next_v->previous = v;
+                next_v->cost = next_cost;
+                pqueue.enqueue(next_v, next_cost);
+            }
+            else if (next_v->getColor() == YELLOW)
+            {
+                double next_cost = v->cost + edge->cost;
+                if (next_cost < next_v->cost)
+                {
+                    next_v->cost = next_cost;
+                    next_v->previous = v;
+                    pqueue.changePriority(next_v, next_v->cost);
+                }
+            }
+        }
+    }
+
+    if (end->getColor() == GREEN)
+    {
+        Vertex* path_v = end;
+        while (path_v->previous)
+        {
+            path.insert(0, path_v);
+            path_v = path_v->previous;
+        }
+        path.insert(0, path_v);
+    }
     return path;
 }
 
 Vector<Vertex*> aStar(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
+
     Vector<Vertex*> path;
     return path;
 }

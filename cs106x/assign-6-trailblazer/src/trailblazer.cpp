8b0ed46bbfeb00425b96225e790649ea96589839
chuzui <chuqi77@gmail.com>
2016-04-15 10:04:06 +0800

    Finish dfs

--- cs106x/assign-6-trailblazer/src/trailblazer.cpp
+++ cs106x/assign-6-trailblazer/src/trailblazer.cpp
@@ -6,17 +6,45 @@
 #include "costs.h"
 #include "trailblazer.h"
 #include "queue.h"
+#include "stack.h"
 #include <map>
 
 // TODO: include any other headers you need; remove this comment
 using namespace std;
 
 Vector<Vertex*> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
+    graph.resetData();
     Vector<Vertex*> path;
+    Stack<Vertex*> q;
+    cout << "asdasdsadsa" << endl;
+    q.push(start);
+    start->visited = true;
+
+    while(!q.isEmpty())
+    {
+        Vertex* v = q.pop();
+
+        if (v == end)
+            break;
+
+        for (auto edge : v->edges)
+        {
+            Vertex* next_v = edge->finish;
+            if (!next_v->visited)
+            {
+                q.push(next_v);
+                next_v->previous = v;
+                next_v->visited = true;
+            }
+        }
+    }
+
+    Vertex* path_v = end;
+    while (path_v && path_v->visited)
+    {
+        path.insert(0, path_v);
+        path_v = path_v->previous;
+    }
     return path;
 }
 
